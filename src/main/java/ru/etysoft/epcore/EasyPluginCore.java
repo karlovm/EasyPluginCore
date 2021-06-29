9d048c69b60407a6efa2be727baa8a68f8974ccb
karlov <51130679+karlovm@users.noreply.github.com>
2021-06-30 00:09:43 +0300

    API version

--- src/main/java/ru/etysoft/epcore/EasyPluginCore.java
+++ src/main/java/ru/etysoft/epcore/EasyPluginCore.java
@@ -7,6 +7,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class EasyPluginCore extends JavaPlugin {
 
     private static EasyPluginCore instance;
+    private static String version = "0.1";
+
+    public static String getAPIVersion() {
+        return version;
+    }
 
     @Override
     public void onEnable() {
